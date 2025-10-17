7d4b61a79c7f9a2cf734c0c14454c131d5075ed9
sektant-gabe <fergabe@pm.me>
2025-10-17 19:57:15 -0300

    first commit

---
+++ src/render/render.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "imgui.h"
+#include "implot.h"
+
+#include "render.h"
+
+void render()
+{
+    if (ImGui::Begin("Main"))
+    {
+        ImGui::Text("ENGINE DOS CRIAS!!!!");
+        ImGui::End();
+    }
+}
